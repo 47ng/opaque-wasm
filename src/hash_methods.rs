838ef7a16561b916fe45b1e079868db3d26687ed
Francois Best <github@francoisbest.com>
2023-01-30 13:09:15 +0100

    chore: Replace NIST P-256 with Ristretto
    Opinionated change for https://github.com/47ng/opaque.

--- src/hash_methods.rs
+++ src/hash_methods.rs
@@ -2,8 +2,8 @@ use opaque_ke::ciphersuite::CipherSuite;
 
 pub struct Default;
 impl CipherSuite for Default {
-    type OprfCs = p256::NistP256;
-    type KeGroup = p256::NistP256;
+    type OprfCs = opaque_ke::Ristretto255;
+    type KeGroup = opaque_ke::Ristretto255;
     type KeyExchange = opaque_ke::key_exchange::tripledh::TripleDh;
     type Ksf = argon2::Argon2<'static>;
 }
